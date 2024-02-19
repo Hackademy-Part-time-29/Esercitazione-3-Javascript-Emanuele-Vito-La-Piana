ecf94dc832dff59d4113746674a21b562aac9748
Emanuele La Piana <emanu360hotmail.it>
2024-02-19 19:00:22 +0100

    Primo commit sulla addizione

---
+++ main.js
@@ -0,0 +1,15 @@
+//traccia tre
+
+//let num1 = prompt("Inserisci il primo numero");
+//let num2 = prompt("Inserisci il secondo numero");
+
+
+//console.log(typeof num1); //cosi' avremo stringa
+//console.log("ciao" + " a tutti"); // concatenazione tra stringhe
+//ma se...
+//console.log("10" - "15"); //fa sottrazione e ti da numero, mentre se avessi messo + è concatenazione tra stringhe e non addizione
+// esempio se avessi scritto
+let num1 = parseInt(prompt("Inserisci il primo numero")); //parseInt serve a convertirlo in un numero, lo fa diventare intero
+let num2 = parseInt(prompt("Inserisci il secondo numero"));
+console.log(num1 + num2);
+
